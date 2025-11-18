58f7bd8a84b90ddfec6bb25c1b8fb0a3a401fab1
NCHSMlearning <mercycherotich2006@gmail.com>
2025-11-18 13:01:06 +0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// config.js - FOR LOCAL DEVELOPMENT ONLY
-window.APP_CONFIG = {
-    SUPABASE_URL: "https://lwhtjozfsmbyihenfunw.supabase.co",
-    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3aHRqb3pmc21ieWloZW5mdW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2NTgxMjcsImV4cCI6MjA3NTIzNDEyN30.7Z8AYvPQwTAEEEhODlW6Xk-IR1FK3Uj5ivZS7P17Wpk",
-    LOCATIONIQ_API_KEY: "pk.cbe61eae35ecbe1d1d682c347d81381c",
-    BUILD_TIME: "local-dev",
-    COMMIT_SHA: "local",
-    ENVIRONMENT: "development"
-};
